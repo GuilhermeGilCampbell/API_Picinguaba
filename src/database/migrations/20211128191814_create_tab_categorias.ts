9faa4efaa793d0dc5530e614110704fc75dcaba0
GuilhermeGilCampbell <30280649+GuilhermeGilCampbell@users.noreply.github.com>
2021-12-10 17:37:33 -0300

    migrations altered and node changed for yarn

--- src/database/migrations/20211128191814_create_tab_categorias.ts
+++ src/database/migrations/20211128191814_create_tab_categorias.ts
@@ -5,6 +5,7 @@ export async function up(knex: Knex): Promise<void> {
     return await knex.schema.createTable('tab_categoria', (table) => {
         table.increments('id_categoria',{primaryKey: true}).unique();
         table.specificType('nome_categoria','VARCHAR(25)').notNullable().unique();
+        table.text('descricao');
     });
 }
 
--- src/database/migrations/20211129144243_create_tab_produto.ts
+++ src/database/migrations/20211129144243_create_tab_produto.ts
@@ -3,20 +3,17 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     return await knex.schema.createTable('tab_produto', (table) => {
         table.increments('id',{primaryKey: true}).unique();
+        table.integer('id_categoria').unsigned();
+        table.foreign('id_categoria').references('id_categoria').inTable('tab_categoria');
         table.specificType('nome','VARCHAR(50)').notNullable();
         table.text('descricao').notNullable();
         table.double('preco');
-        table.boolean('destaque').notNullable();
-        table.specificType('longitude','VARCHAR(30)').notNullable();
-        table.specificType('latitude','VARCHAR(30)').notNullable();
-        table.boolean('mostrar_mapa').notNullable();
+        table.boolean('destaque').defaultTo(0);
+        table.text('imagem');
         table.integer('id_contato').unsigned();
         table.foreign('id_contato').references('id').inTable('tab_contato');
         table.boolean('mostrar_contato').notNullable();
-        table.integer('id_categoria').unsigned();
-        table.foreign('id_categoria').references('id_categoria').inTable('tab_categoria');
     });
-
     
 }
 
